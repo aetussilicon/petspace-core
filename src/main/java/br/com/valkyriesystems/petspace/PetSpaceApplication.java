247e3daf7acf7a35d0bee01f3b8317412966d368
Artur Gomes <arturgaollidev@outlook.com>
2025-01-13 21:28:12 -0300

    Initial Commit

---
+++ src/main/java/br/com/valkyriesystems/petspace/PetSpaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.valkyriesystems.petspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/valkyriesystems/petspace/PetSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valkyriesystems.petspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
